27082d5e60cc3e0d06bd2c2475f3710d1e134aa8
林小兀 <linxiaowu@linxiaowudeMacBook-Pro.local>
2019-06-21 14:57:43 +0800

    feat(ignore): ignore build folder

--- packages/md5-NAPI/build/Release/.deps/Release/md5-napi.node.d
+++
@@ -1,1 +0,0 @@
-cmd_Release/md5-napi.node := c++ -bundle -undefined dynamic_lookup -Wl,-no_pie -Wl,-search_paths_first -mmacosx-version-min=10.7 -arch x86_64 -L./Release -stdlib=libc++  -o Release/md5-napi.node Release/obj.target/md5-napi/src/init.o Release/nothing.a -lssl -lcrypto
